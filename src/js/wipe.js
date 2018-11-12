c187271c294de1c4c56f4a2f0230fa6f7dfe1980
haotata168 <58021125@qq.com>
2018-11-12 22:03:12 +0800

    支持移动端

--- src/js/wipe.js
+++ src/js/wipe.js
@@ -7,6 +7,11 @@ var moveY = 0;
 var isMouseDown = false;
 //表示鼠标的状态，是否按下，默认为未按下false，按下true
 
+//device保存设备类型，如果是移动端则为true，PC端为false
+var device = (/android|webos|iPhone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+console.log( navigator.userAgent );
+console.log( device );
+
 //生成画布上的遮罩，默认为颜色#666
 function drawMask(context){
 	context.fillStyle="#666";
@@ -32,6 +37,9 @@ function drawLine(context,x1,y1,x2,y2){
 	context.stroke();
 	context.restore();
 }
+
+//cas.addEventListener(eventClick,function(evt){},false)
+
 //在canvas画布上监听自定义事件"mousedown"，调用drawPoint函数
 cas.addEventListener("mousedown",function(evt){
 	isMouseDown = true;
@@ -41,9 +49,18 @@ cas.addEventListener("mousedown",function(evt){
 	moveY = event.clientY;
 	drawPoint(context,moveX,moveY);
 },false);
+//为画布添加手势操作--手指点击响应
+cas.addEventListener("touchstart",function(evt){
+	isMouseDown = true;
+	var event = evt || window.event;
+	//获取手指在视口的坐标，传递参数到drawPoint
+	moveX = event.touches[0].clientX;
+	moveY = event.touches[0].clientY;
+	drawPoint(context,moveX,moveY);
+},false);
+
 //增加监听"mousemove",调用drawPoint函数
-cas.addEventListener("mousemove",fn1,false);
-function fn1(evt){
+cas.addEventListener("mousemove",function(evt){
 	//判断，当isMouseDown为true时，才执行下面的操作
 	if( !isMouseDown ){
 		return false;
@@ -57,8 +74,28 @@ function fn1(evt){
 		moveX = x2;
 		moveY = y2;
 	}
-}
-cas.addEventListener("mouseup",function(){
+},false);
+ //手指移动
+cas.addEventListener("touchmove",function(evt){
+	//判断，当isMouseDown为true时，才执行下面的操作
+	if( !isMouseDown ){
+		return false;
+	}else{
+		var event = evt || window.event;
+		event.preventDefault();
+		var x2 = event.touches[0].clientX;
+		var y2 = event.touches[0].clientY;
+		//drawPoint(context,a,b);
+		drawLine(context,moveX,moveY,x2,y2);
+		//每次的结束点变成下一次划线的开始点
+		moveX = x2;
+		moveY = y2;
+	}
+},false);
+
+cas.addEventListener("mouseup",fn2,false);
+cas.addEventListener("touchend",fn2,false);
+function fn2(){
 	//还原isMouseDown 为false
 	isMouseDown = false;
 	if( getTransparencyPercent(context) > 50){
@@ -66,7 +103,7 @@ cas.addEventListener("mouseup",function(){
 		//drawMask(context);
 		clearRect(context);
 	}
-},false);
+}
 
 function clearRect(context){
 	context.clearRect(0,0,_w,_h);
